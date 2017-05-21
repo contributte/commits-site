45831f4d4c5db1c15f54ed5182d9be983e3698ae
Petr Kessler <kesspess@gmail.com>
2017-05-21 17:23:09 +0200

    added entities

---
+++ app/Entity/Commit.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Entity;
+
+
+class Commit
+{
+
+	/** @var Repository */
+	private $repository;
+
+	/** @var string */
+	private $sha;
+
+	/** @var User|null */
+	private $author;
+
+	/** @var string */
+	private $authorName;
+
+	/** @var \DateTimeImmutable */
+	private $authoredAt;
+
+	/** @var User|null */
+	private $committer;
+
+	/** @var string */
+	private $committerName;
+
+	/** @var \DateTimeImmutable */
+	private $committedAt;
+
+	/** @var string */
+	private $url;
+
+	/** @var string */
+	private $message;
+
+	/** @var int */
+	private $additions;
+
+	/** @var int */
+	private $deletions;
+
+	/** @var int */
+	private $total;
+
+
+	public function __construct(
+		Repository $repository,
+		string $sha,
+		?User $author,
+		string $authorName,
+		\DateTimeImmutable $authoredAt,
+		?User $committer,
+		string $committerName,
+		\DateTimeImmutable $committedAt,
+		string $url,
+		string $message,
+		int $additions,
+		int $deletions,
+		int $total
+
+	) {
+		$this->sha = $sha;
+		$this->url = $url;
+		$this->total = $total;
+		$this->author = $author;
+		$this->message = $message;
+		$this->additions = $additions;
+		$this->committer = $committer;
+		$this->deletions = $deletions;
+		$this->repository = $repository;
+		$this->authorName = $authorName;
+		$this->authoredAt = $authoredAt;
+		$this->committedAt = $committedAt;
+		$this->committerName = $committerName;
+	}
+
+}
