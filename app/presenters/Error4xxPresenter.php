48319a6daadabab5dc2a4623fbac42c8be437a59
Petr Kessler <kesspess@gmail.com>
2017-05-21 12:22:18 +0200

    init commit (started from nette/web-project)

---
+++ app/presenters/Error4xxPresenter.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Presenters;
+
+use Nette\Application\Request;
+use Nette\Application\UI\Presenter;
+use Nette\Application\BadRequestException;
+use Nette\Bridges\ApplicationLatte\Template;
+
+
+/** @property Template $template */
+final class Error4xxPresenter extends Presenter
+{
+
+	public function startup(): void
+	{
+		parent::startup();
+
+		$request = $this->getRequest();
+
+		if (!$request || !$request->isMethod(Request::FORWARD)) {
+			$this->error();
+		}
+	}
+
+
+	public function renderDefault(BadRequestException $exception): void
+	{
+		// load template 403.latte or 404.latte or ... 4xx.latte
+		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
+		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+	}
+
+}
