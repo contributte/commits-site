b7b44d739f8371d011cb73143c064103b69da6c6
Petr Kessler <kesspess@gmail.com>
2017-05-21 12:22:18 +0200

    init commit (started from nette/web-project)

---
+++ app/router/RouterFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App;
+
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+
+final class RouterFactory
+{
+
+	/** @return RouteList<Route> */
+	public static function createRouter(): RouteList
+	{
+		$router = new RouteList;
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		return $router;
+	}
+
+}
