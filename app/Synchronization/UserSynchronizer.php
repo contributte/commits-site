95b8483a896004686fd22ac3192ac5661e6d8ea2
Petr Kessler <kesspess@gmail.com>
2017-05-22 12:59:55 +0200

    added synchronization services

---
+++ app/Synchronization/UserSynchronizer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Synchronization;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Facade\User\UserPersister;
+use App\Entity\Synchronization\RepositoryLog;
+use App\QueryFunction\User\UsersIndexedByGithubIdQuery;
+
+
+final class UserSynchronizer
+{
+
+	/** @var UsersIndexedByGithubIdQuery */
+	private $usersQuery;
+
+	/** @var UserPersister */
+	private $userPersister;
+
+	/** @var User[] */
+	private $users;
+
+
+	public function __construct(UsersIndexedByGithubIdQuery $usersQuery, UserPersister $userPersister)
+	{
+		$this->usersQuery = $usersQuery;
+		$this->userPersister = $userPersister;
+	}
+
+
+	public function synchronize(int $githubID, string $login, ?string $avatarURL): User
+	{
+		if ($this->users === null) {
+			$this->users = $this->usersQuery->get();
+		}
+
+		if (isset($this->users[$githubID])) {
+			$local = $this->users[$githubID];
+			$local->merge($login, $avatarURL);
+
+		} else {
+			$local = new User($githubID, $login, $avatarURL);
+		}
+
+		$this->userPersister->persist($local);
+		$this->users[$githubID] = $local;
+
+		return $local;
+	}
+
+}
