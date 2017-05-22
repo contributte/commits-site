fb6c46b5d63d7c9db67db68cdb5148ff6cc5d2d8
Petr Kessler <kesspess@gmail.com>
2017-05-22 09:04:18 +0200

    added entities mapping & associations

--- app/Entity/CommitFile.php
+++ app/Entity/CommitFile.php
@@ -5,27 +5,58 @@ declare(strict_types = 1);
 namespace App\Entity;
 
 use Ramsey\Uuid\Uuid;
+use Doctrine\ORM\Mapping as ORM;
 
 
+/** @ORM\Entity */
 class CommitFile
 {
 
-	/** @var string */
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $id;
 
-	/** @var string */
+	/**
+	 * @ORM\ManyToOne(targetEntity = "Commit", inversedBy = "files")
+	 * @ORM\JoinColumns({
+	 *     @ORM\JoinColumn(name = "commit_repository", referencedColumnName = "repository", nullable = false, onDelete = "CASCADE"),
+	 *     @ORM\JoinColumn(name = "commit_sha", referencedColumnName = "sha", nullable = false, onDelete = "CASCADE")
+	 * })
+	 * @var Commit
+	 */
+	private $commit;
+
+	/**
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $filename;
 
-	/** @var string */
+	/**
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $status;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $additions;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $deletions;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $changes;
 
 
@@ -36,6 +67,7 @@ class CommitFile
 
 
 	public function __construct(
+		Commit $commit,
 		string $filename,
 		string $status,
 		int $additions,
@@ -43,7 +75,12 @@ class CommitFile
 		int $changes
 	) {
 		$this->id = Uuid::uuid4()->toString();
+
+		$this->commit = $commit;
+		$commit->addFile($this);
+
 		$this->filename = $filename;
+
 		$this->status = $status;
 		$this->additions = $additions;
 		$this->deletions = $deletions;
