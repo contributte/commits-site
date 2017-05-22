3090a4df24c486e307f8a4ad5119cea35830d401
Petr Kessler <kesspess@gmail.com>
2017-05-22 09:04:18 +0200

    added entities mapping & associations

--- app/Entity/Commit.php
+++ app/Entity/Commit.php
@@ -4,45 +4,113 @@ declare(strict_types = 1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
+
+/**
+ * @ORM\Entity
+ *
+ * @ORM\Table(indexes = {
+ *     @ORM\Index(columns = {"committed_at"}),
+ *     @ORM\Index(columns = {"sort"})
+ * })
+ */
 class Commit
 {
 
-	/** @var Repository */
+	/**
+	 * @ORM\Id
+	 * @ORM\ManyToOne(targetEntity = "Repository", inversedBy = "commits")
+	 * @ORM\JoinColumn(name = "repository", referencedColumnName = "id", onDelete = "CASCADE")
+	 *
+	 * @var Repository
+	 */
 	private $repository;
 
-	/** @var string */
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $sha;
 
-	/** @var User|null */
+	/**
+	 * @ORM\ManyToOne(targetEntity = "User")
+	 * @ORM\JoinColumn(name = "author", onDelete = "SET NULL")
+	 * @var User|null
+	 */
 	private $author;
 
-	/** @var string */
+	/**
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $authorName;
 
-	/** @var \DateTimeImmutable */
+	/**
+	 * @ORM\Column(type = "datetime_immutable")
+	 * @var \DateTimeImmutable
+	 */
 	private $authoredAt;
 
-	/** @var User|null */
+	/**
+	 * @ORM\ManyToOne(targetEntity = "User")
+	 * @ORM\JoinColumn(name = "committer", onDelete = "SET NULL")
+	 * @var User|null
+	 */
 	private $committer;
 
-	/** @var string */
+	/**
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
 	private $committerName;
 
-	/** @var \DateTimeImmutable */
+	/**
+	 * @ORM\Column(type = "datetime_immutable")
+	 * @var \DateTimeImmutable
+	 */
 	private $committedAt;
 
-	/** @var string */
+
+	/**
+	 * @ORM\Column(type = "text")
+	 * @var string
+	 */
 	private $message;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $additions;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $deletions;
 
-	/** @var int */
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
 	private $total;
+
+	/**
+	 * @ORM\OneToMany(targetEntity = "CommitFile", mappedBy = "commit", cascade = {"persist"})
+	 * @ORM\OrderBy({"filename" = "ASC"})
+	 * @var CommitFile[]|Collection<int, CommitFile>
+	 */
+	private $files;
+
+	/**
+	 * @ORM\Column(type = "integer")
+	 * @var int
+	 */
+	private $sort = 0;
 
 
 	public function __construct(
@@ -61,17 +129,43 @@ class Commit
 
 	) {
 		$this->sha = $sha;
+		$repository->addCommit($this);
+		$this->repository = $repository;
+
+		$this->author = $author;
+		$this->authoredAt = $authoredAt;
+		$this->authorName = $authorName;
+
+		$this->committer = $committer;
+		$this->committedAt = $committedAt;
+		$this->committerName = $committerName;
+
 		$this->total = $total;
 		$this->author = $author;
 		$this->message = $message;
+
+		$this->total = $total;
 		$this->additions = $additions;
 		$this->committer = $committer;
 		$this->deletions = $deletions;
-		$this->repository = $repository;
-		$this->authorName = $authorName;
-		$this->authoredAt = $authoredAt;
-		$this->committedAt = $committedAt;
-		$this->committerName = $committerName;
+
+		$this->files = new ArrayCollection;
+	}
+
+
+	public function addFile(CommitFile $file): self
+	{
+		if (!$this->hasFile($file)) {
+			$this->files->add($file);
+		}
+
+		return $this;
+	}
+
+
+	public function hasFile(CommitFile $file): bool
+	{
+		return $this->files->contains($file);
 	}
 
 }
