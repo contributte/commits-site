d0aac2bb3033534ee23298fc26e90bdd3c17e27d
Petr Kessler <kesspess@gmail.com>
2017-05-22 09:04:18 +0200

    added entities mapping & associations

--- app/Entity/Repository.php
+++ app/Entity/Repository.php
@@ -4,14 +4,49 @@ declare(strict_types = 1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
 
+
+/** @ORM\Entity */
 class Repository
 {
 
-	/** @var Project */
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type = "string")
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity = "Project", inversedBy = "repositories")
+	 * @ORM\JoinColumn(nullable = false, onDelete = "CASCADE")
+	 * @var Project
+	 */
 	private $project;
 
-	/** @var string */
-	private $name;
+	/**
+	 * @ORM\OneToMany(targetEntity = "Commit", mappedBy = "repository")
+	 * @ORM\OrderBy({"committedAt" = "DESC"})
+	 * @var Commit[]|Collection<int, Commit>
+	 */
+	private $commits;
+
+
+	public function addCommit(Commit $commit): self
+	{
+		if (!$this->hasCommit($commit)) {
+			$this->commits->add($commit);
+		}
+
+		return $this;
+	}
+
+
+	public function hasCommit(Commit $commit): bool
+	{
+		return $this->commits->contains($commit);
+	}
 
 }
