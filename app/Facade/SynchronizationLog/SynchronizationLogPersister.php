430af732e9c1bd870ea643e8110d3dbd97887298
Petr Kessler <kesspess@gmail.com>
2017-05-23 02:03:00 +0200

    added synchronization script with result logging

---
+++ app/Facade/SynchronizationLog/SynchronizationLogPersister.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Facade\SynchronizationLog;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Synchronization\SynchronizationLog;
+
+
+final class SynchronizationLogPersister
+{
+
+	/** @var EntityManagerInterface */
+	private $em;
+
+
+	public function __construct(EntityManagerInterface $em)
+	{
+		$this->em = $em;
+	}
+
+
+	public function persist(SynchronizationLog $log): void
+	{
+		$this->em->persist($log);
+		$this->em->flush();
+	}
+
+}
