08779b6451cf0fd0dabc87373e5c1435e3b55b81
Petr Kessler <kesspess@gmail.com>
2017-05-31 11:50:12 +0200

    added commits datagrid

---
+++ app/Helper/TimeAgoInWordsHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Helper;
+
+use Nette\StaticClass;
+
+
+final class TimeAgoInWordsHelper
+{
+
+	/** @author David Grudl */
+	public static function convert(\DateTimeInterface $datetime): ?string
+	{
+		$time = (int) $datetime->format('U');
+		$delta = time() - $time;
+
+		if ($delta < 0) {
+			return null;
+		}
+
+		$delta = (int) round($delta / 60);
+		if ($delta === 0) return 'just now';
+		if ($delta === 1) return 'a minute ago';
+		if ($delta < 45) return "$delta minutes ago";
+		if ($delta < 90) return 'an hour ago';
+		if ($delta < 1440) return round($delta / 60) . ' hours ago';
+		if ($delta < 2880) return 'yesterday';
+		if ($delta < 43200) return round($delta / 1440) . ' days ago';
+		if ($delta < 86400) return 'a month ago';
+		if ($delta < 525960) return round($delta / 43200) . ' months ago';
+		if ($delta < 1051920) return 'a year ago';
+		return round($delta / 525960) . ' years ago';
+	}
+
+}
