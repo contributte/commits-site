164a7bd4e689b91c1ab1fc4105325d03da2f7a12
Petr Kessler <kesspess@gmail.com>
2017-06-03 11:50:20 +0200

    added RSS feeds

---
+++ app/Presenter/ProjectRssPresenter.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Presenter;
+
+use Nette\Application\UI\Presenter;
+use App\QueryFunction\Project\ProjectBySlugQuery;
+use App\QueryFunction\Project\ProjectNotFoundException;
+use App\QueryFunction\Commit\CommitsLatestByProjectWithFilesQuery;
+
+
+final class ProjectRssPresenter extends Presenter
+{
+
+	/** @var ProjectBySlugQuery */
+	private $projectBySlugQuery;
+
+	/** @var CommitsLatestByProjectWithFilesQuery */
+	private $commitsQuery;
+
+
+	public function __construct(ProjectBySlugQuery $projectBySlugQuery, CommitsLatestByProjectWithFilesQuery $commitsQuery)
+	{
+		parent::__construct();
+
+		$this->commitsQuery = $commitsQuery;
+		$this->projectBySlugQuery = $projectBySlugQuery;
+	}
+
+
+	public function renderFeed(string $projectSlug): void
+	{
+		try {
+			$project = $this->projectBySlugQuery->get($projectSlug);
+
+			$this->template->project = $project;
+			$this->template->commits = $this->commitsQuery->get($project, 60);
+
+		} catch (ProjectNotFoundException $e) {
+			$this->error($e->getMessage());
+		}
+	}
+
+}
