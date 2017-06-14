7f0e74b36211b14be2bc4d57fff779cb28feb99c
Petr Kessler <kesspess@gmail.com>
2017-06-14 03:04:19 +0200

    added responsive design

--- www/assets/script.js
+++ www/assets/script.js
@@ -5,11 +5,35 @@
 
 	$(function () {
 
+		var spinner = $('#spinner');
+
+		$.nette.ext('spinner', {
+			start: function () {
+				spinner.show();
+			},
+
+			complete: function () {
+				spinner.hide();
+			}
+		});
+
 		$.nette.ext('clipboard', {
 			load: function () {
 				$('[data-clipboard-text]').each(function () {
 					new Clipboard(this);
 				});
+			}
+		});
+
+		$.nette.ext('filters', {
+			success: function (payload, status, jqXHR, settings) {
+				if (settings.nette && settings.nette.el) {
+					var name = settings.nette.el.attr('name');
+
+					if (name && name.indexOf('filters[buttons]') !== -1) {
+						$('#commits-header').collapse('hide');
+					}
+				}
 			}
 		});
 
