fe3c5c52f3678c38cd9d7c5752a295c6cdf735b6
Petr Kessler <kesspess@gmail.com>
2018-05-06 16:15:30 +0200

    added CI pipeline - phplint, phpstan, code-checker & tester

---
+++ tests/Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests;
+
+use Tester\Environment;
+use Nette\Configurator;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+final class Bootstrap
+{
+
+	public static function boot(): Configurator
+	{
+		Environment::setup();
+		return \App\Bootstrap::boot();
+	}
+
+}
